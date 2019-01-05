a158347e3e362840a6e5ab1c6464d2d593b8c7a1
zhoufeifan <937127883@qq.com>
2019-01-05 12:21:05 +0800

    Update getBitchUserInfo.js

--- js/getBitchUserInfo.js
+++ js/getBitchUserInfo.js
@@ -39,7 +39,16 @@ function requestUserProfile (uidList){  // uidList 是一个数组，最大接�
     });
   });
 }
+// 现在我们有很多业务都需要根据 uid 获取 userProfile , 大多数业务的需求都是给一个 uid，获取 profile 。
+// 为了性能，我们需要把这个单个的请求合并成批量请求。
 
+// 例如，现在页面上 A 模块需要获取 uid 为 1 的 profile，B 模块需要 uid 为 2 的 profile，
+// C 模块需要获取 uid 为 1 的profile 这三个模块会单独调用下面这个方法获取 profile，
+// 假设这三次调用的时间非常接近(100ms 以内)，最终要求只发送一个 ajax 请求（只调用一次 requestUserProfile )，
+// 拿到这三个模块需要的 profile
+
+// 完成以下方法，接收一个参数 uid，返回一个 Promise，当成功请求到 profile 的时候， resolve 对应的profile , 请求失败 reject
+// 例如  getUserProfile(1).then(function(profile){ console.log(profile.uid === 1) // true });  // 假设请求成功了。
 
 var getUserProfile = function (uid){
   function RequestProxy(){
