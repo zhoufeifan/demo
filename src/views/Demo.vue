2dd7cae1b1b980fb83492ec110dd4d0418f037cd
zhoufeifan <zhoufeifan@souche.com>
2019-06-04 15:09:38 +0800

    3d demo

--- src/views/Demo.vue
+++ src/views/Demo.vue
@@ -3,32 +3,20 @@ import { mapState } from 'vuex'
 import Vue from 'vue'
   export default {
     name: 'demo',
-    components: {
-
-    },
-    created(){
-    },
-    mounted(){
-    },
-    computed: {
-    },
-    methods: {
-      nima() {
-        this.aaa = 'nn'
-      }
-    },
-    data(){
-      return {
-        aaa: 'aa'
-      }
-    },
     render(){
-      const aa = 'nima'
-      const tag = '<span>fd</span>'
       return(
         <div>
-          {aa}
-          <tag/>
+          <script src="https://cdn.scaleflex.it/plugins/js-cloudimage-360-view/2/js-cloudimage-360-view.min.js"/>
+          <div
+              class="cloudimage-360"
+              data-folder="https://scaleflex.cloudimg.io/crop/1920x700/n/https://scaleflex.airstore.io/demo/360-car/"
+              data-filename="iris-{index}.jpeg"
+              data-amount="18"
+              autoplay
+              autoplay-reverse
+              data-full-screen
+              data-magnifier="3"
+          ></div>
         </div>
         )
     }
